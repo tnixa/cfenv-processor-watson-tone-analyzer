a229b9675365c5a5caa82222b3342e41cf1cd2ce
Terrence Nixa <tnixa@us.ibm.com>
2019-05-20 16:50:24 -0500

    initial commit

--- src/main/java/org/terrence/cfenvprocessors/watsontoneanalyser/WatsonToneAnalyzerCfEnvProcessor.java
+++ src/main/java/org/terrence/cfenvprocessors/watsontoneanalyser/WatsonToneAnalyzerCfEnvProcessor.java
@@ -1,4 +1,4 @@
-package org.terrence.testapp.envprocessor;
+package org.terrence.cfenvprocessors.watsontoneanalyser;
 
 import java.util.Map;
 
@@ -29,8 +29,8 @@ public class WatsonToneAnalyzerCfEnvProcessor implements CfEnvProcessor {
     @Override
     public void process(CfCredentials cfCredentials, Map<String, Object> properties) {
         // set watsonVersion to date of the released watson spring boot starter
-        // version 0.3.0 was released on 2018-06-22
-        String watsonVersion = "2018-06-22";
+        // version 1.0.0 was released on 2019-05-07
+        String watsonVersion = "2019-05-07";
         properties.put("watson.tone-analyzer.url", cfCredentials.getUri("http"));
         properties.put("watson.tone-analyzer.iam-api-key", cfCredentials.getString("apikey"));
         properties.put("watson.tone-analyzer.versionDate", watsonVersion);
